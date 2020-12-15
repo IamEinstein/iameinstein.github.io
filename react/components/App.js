6386b0719a973b917bf118a3304ba95dd9e95720
Rishit <vermarishit@gmail.com>
2020-12-15 15:16:06 +0530

    Basic Page

---
+++ react/components/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 0,
+    padding: 0,
+    margin: 0,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Coming Soon , English Project....
+          </Typography>
+          <Button color="inherit">Soon</Button>
+        </Toolbar>
+      </AppBar>
+      <br />
+      <br />
+      <Typography variant="h3" className={classes.title} align="center">
+        Coming Soon , English Project....
+      </Typography>
+    </div>
+  );
+}
+
+// export default App;
